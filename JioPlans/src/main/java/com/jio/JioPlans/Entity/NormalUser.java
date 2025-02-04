7845c4a3ec2232702d4b02cf1fbfab9a405ff90e
Mahi3333 <mumamahesh13@gmail.com>
2025-02-04 15:28:30 -0500

    I am commiting some changes to the repository and adding the Users email address.

--- JioPlans/src/main/java/com/jio/JioPlans/Entity/NormalUser.java
+++ JioPlans/src/main/java/com/jio/JioPlans/Entity/NormalUser.java
@@ -54,4 +54,5 @@ public class NormalUser {
     private String normalPlanType;
     private String normalUserLastName;
     private String normalUserFirstName;
+    private String normalUserEmail;
 }
