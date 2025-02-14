6337fc8208a3c33ac7d3180e78576c2be7410012
Mahi3333 <mumamahesh13@gmail.com>
2025-02-14 15:22:28 -0500

    Inntegrated kafka with the microservices CorporateUserController, NormalUserController.java and plans.

--- JioPlans/src/main/java/com/jio/JioPlans/Controller/NormalUserController.java
+++ JioPlans/src/main/java/com/jio/JioPlans/Controller/NormalUserController.java
@@ -2,6 +2,7 @@ package com.jio.JioPlans.Controller;
 
 import com.jio.JioPlans.DTO.NUdto;
 import com.jio.JioPlans.Entity.NormalUser;
+import com.jio.JioPlans.Service.KafkaProducerService;
 import com.jio.JioPlans.Service.NormalUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,20 @@ public class NormalUserController {
 
     @Autowired
     public NormalUserService normService;
+
+    @Autowired
+    private final KafkaProducerService kafkaProducerService;
+
+    public NormalUserController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @PostMapping("/selectPlan")
+    public String selectPlan(@RequestParam String userName, @RequestParam String planName){
+        String message = "Normal User "+ userName+" selected plan "+planName;
+        kafkaProducerService.sendMessage("plans-topic",message);
+        return "Plan selection published";
+    }
 
     @GetMapping("/testing")
     public String testingOutput(){
